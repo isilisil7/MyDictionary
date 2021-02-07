b69af09ead14130d7727fd9808813f61d94c0e60
isilisil7 <579+isilisil7@users.noreply.github.com>
2021-02-07 10:07:36 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T,V>
+    {
+        T[] items;
+        V[] values;
+        public MyDictionary()
+        {
+            items = new T[0];
+            values = new V[0];
+        }
+
+        public void Add(T item, V value)
+        {
+            T[] tempArray = items;
+            V[] tempValue = values;
+
+            items = new T[items.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                tempArray[i] = items[i];
+            }
+
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                tempValue[i] = values[i];
+            }
+
+            items[items.Length - 1] = item;
+            values[values.Length - 1] = value;
+
+            Console.WriteLine("item: [" + item + "] " + " value : [" + value + "]");
+
+        }
+        public int LengthItems
+        {
+            get { return items.Length; }
+        }
+
+        public int LengthValues
+        {
+            get { return values.Length; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> info = new MyDictionary<string, string>();
+            Console.WriteLine("Program basladi, constructor calisti. Eleman sayisi: ");
+            Console.WriteLine("items length: " + info.LengthItems);
+            Console.WriteLine("values length: " + info.LengthValues);
+            info.Add("name", "isil");
+            info.Add("surname", "ozkaynak");
+            Console.WriteLine("items length: " + info.LengthItems);
+            Console.WriteLine("values length: " + info.LengthValues);
+            info.Add("color", "blue");
+            info.Add("number", "9999");
+            Console.WriteLine("items length: " + info.LengthItems);
+            Console.WriteLine("values length: " + info.LengthValues);
+        }
+    }
+}
