7efbf947a702d2bd1ed24e65bfbafb58c27bd894
isilisil7 <579+isilisil7@users.noreply.github.com>
2021-02-07 10:42:23 +0300

    C# Kampı 4.Gün Ödev5

--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -10,8 +10,8 @@ namespace MyDictionary
             Console.WriteLine("Program basladi, constructor calisti. Eleman sayisi: ");
             Console.WriteLine("items length: " + info.LengthItems);
             Console.WriteLine("values length: " + info.LengthValues);
-            info.Add("name", "isil");
-            info.Add("surname", "ozkaynak");
+            info.Add("name", "ayse");
+            info.Add("surname", "yilmaz");
             Console.WriteLine("items length: " +  info.LengthItems);
             Console.WriteLine("values length: " + info.LengthValues);
             info.Add("color", "blue");
